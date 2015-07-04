7a3582d8b6bd5dad7f2ced40e4d9976985b59df2
Maedi <maediprichard@gmail.com>
2015-07-04 16:34:11 +1000

    jquery fun

---
+++ js/README.md
@@ -0,0 +1,26 @@
+# History Repeats
+
+Google Doc: https://docs.google.com/document/d/1tDLcLSWPdYXsjMtJrFBgG4mcN71Qbhk1N5rbitEITJw/edit#
+
+**Git commands**
+
+git pull
+git commit -am "my change"
+git push
+git pull --rebase origin master
+
+## Workflow
+
+1. User inputs keywords
+2. Query Trove API using keywords
+3. Filter returned results:  
+   - Remove unrelated events
+   - Remove duplicate events (multiple articles may reference the same event)
+4. Import filtered results into Event model
+5. Calculate average occurrence of Events
+6. Display Events
+
+## Trove API
+
+**Example HTTP query**
+http://api.trove.nla.gov.au/result?q=death%20AND%20by%20AND%20lion&zone=newspaper&encoding=json&key=u944in30nism514v
---
+++ js/main.js
@@ -0,0 +1,48 @@
+//var url = "http://api.trove.nla.gov.au/result?q=death%20AND%20by%20AND%20lion&zone=newspaper&encoding=json&callback=?";
+var api = "http://api.trove.nla.gov.au/result";
+var apiKey = "u944in30nism514v";
+var searchZone = 'newspaper';
+var query = "death%20AND%20by%20AND%20lion&zone=newspaper&encoding=json&key=u944in30nism514v";
+
+var searchTerm = "death AND by AND lion";
+
+// action that occurs when the 'Search' button is clicked
+$("#searchbtn").bind("click", function() {
+
+    // Get the values from our search form
+    //var searchZone = $("#searchZone").val();
+    var sortBy = $("#sortBy").val();
+
+    // Construct the URL for the Trove Search API
+    var url = "http://api.trove.nla.gov.au/result?key="
+        + apiKey + "&encoding=json&zone=" + searchZone
+        + "&sortby=" + sortBy
+        + "&q=" + searchTerm + "&callback=?";
+
+    // Get the results as JSON and display
+    $.getJSON(url, function(data) {
+        console.log(data);
+
+        //var displayJSON = function( index, item ) {
+
+        //    $('#output').append("<li>" + (index + 1) + ". "
+        //      // display link to record
+        //      +"<a target='_blank' href='" + item.troveUrl +"'>"
+        //      + (item.title.value || item.title || item.name)
+        //      + "</a><br/>"
+
+        //      // display JSON response (formatted)
+        //      + "<pre>" + JSON.stringify(item,null,4) + "</pre>"
+        //      + "</li>");
+
+        //};
+        //$('#output').empty();
+        //if (data.response.zone[0].records.work){
+        //    $.each(data.response.zone[0].records.work, displayJSON);
+        //} else if (data.response.zone[0].records.article){
+        //    $.each(data.response.zone[0].records.article, displayJSON);
+        //} else if (data.response.zone[0].records.list){
+        //    $.each(data.response.zone[0].records.list, displayJSON);
+        //}
+    });
+});
